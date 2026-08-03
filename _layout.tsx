203e60e0fb6c0787232ff40f255ec61ace387f7e
ercanuysalcan-dot <ercanuysalcan@gmail.com>
2026-08-04 01:06:05 +0300

    Add files via upload

---
+++ _layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import { AuthProvider } from "@/hooks/useAuth";
+
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </AuthProvider>
+  );
+}
